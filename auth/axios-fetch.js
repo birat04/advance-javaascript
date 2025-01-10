146497d7f7179c708eb44162a014e072b687931e
Birat Roka <23btrcn071@jainuniversity.ac.in>
2025-01-10 14:26:41 +0530

    updated

--- auth/axios-fetch.js
+++ auth/axios-fetch.js
@@ -1,17 +1,28 @@
-const axios = require('axios');
-async function main(){
-    const response = await fetch("https://sum-server.100xdevs.com/todos");
-    const json = await response.json();
-    console.log(json.todos.length)
+// const axios = require('axios');
+// async function main(){
+//     const response = await fetch("https://sum-server.100xdevs.com/todos");
+//     const json = await response.json();
+//     console.log(json.todos.length)
 
+// }
+// main();
+
+
+// async function main(){
+//     const response = await axios.get("https://sum-server.100xdevs.com/todos");
+//     const json = await response.json();
+//     console.log(json.todos.length)
+
+// }
+// main();
+
+
+
+const jwt = require("jsonwebtoken");
+
+const value = {
+    name: "Birat",
+    accountNumber: 123123123
 }
-main();
-
-
-async function main(){
-    const response = await axios.get("https://sum-server.100xdevs.com/todos");
-    const json = await response.json();
-    console.log(json.todos.length)
-
-}
-main();
+const token = jwt.sign(value,"secret");
+console.log(token);
--- middleware2.js
+++ middleware2.js
@@ -15,16 +15,65 @@
 // app.get("/divide",realSumHandeler);
 // app.listen(3000);
 
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-app.use(express.json());
+// app.use(express.json());
 
-app.post("/sum",function(req,res) {
-    const a = parseInt(req.body.a);
-    const b = parseInt(req.body.b);
-    res.json({
-        ans: a+b
-    })
-});
-app.listen(3000);
+// app.post("/sum",function(req,res) {
+//     const a = parseInt(req.body.a);
+//     const b = parseInt(req.body.b);
+//     res.json({
+//         ans: a+b
+//     })
+// });
+// app.listen(3000);
+
+
+const jwt = require("jsonwebtoken");
+const zod = require("zod");
+
+const JWT_SECRET = "birat"; 
+
+
+const emailSchema = zod.string().email();
+const passwordSchema = zod.string().min(6);
+
+
+function signJWT(username, password) {
+    const usernameResponse = emailSchema.safeParse(username);
+    const passwordResponse = passwordSchema.safeParse(password);
+
+    if (!usernameResponse.success || !passwordResponse.success) {
+        return null; 
+    }
+
+    const signature = jwt.sign({ username }, JWT_SECRET);
+    return signature;
+}
+
+
+const token = signJWT("birat@example.com", "securepassword");
+console.log("Generated Token:", token);
+
+
+function verifyJWT(token) {
+    try {
+        jwt.verify(token, JWT_SECRET);
+        return true; 
+    } catch (e) {
+        return false; 
+    }
+}
+
+
+console.log("Is Token Valid?", verifyJWT(token));
+
+
+function decodeJWT(token) {
+    const decoded = jwt.decode(token);
+    return decoded ? decoded : null; 
+}
+
+
+console.log("Decoded Token:", decodeJWT(token));
